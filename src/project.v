34148ba49f20721c12c99189027c75c12b5bcde5
Jeffrey Kang <tk3172@nyu.edu>
2024-10-23 19:43:59 -0400

    array_multiplier

--- src/project.v
+++ src/project.v
@@ -8,7 +8,6 @@
 module tt_um_LabA_Group11 (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
     output wire [7:0] uio_out,  // IOs: Output path
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
     input  wire       ena,      // always 1 when the design is powered, so you can ignore it
@@ -87,4 +86,4 @@ endmodule
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
 
-endmodule
+
